cf1e7d8e9262228159e91d44d07fe35a2f5ca308
fatmaikis <fatmaikis1999@gmail.com>
2022-12-09 13:49:15 +0300

    Add project files.

---
+++ OdevDizi/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace OdevDizi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.kullaniciaditxt = new System.Windows.Forms.TextBox();
+            this.sifretxt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(189, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(267, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Şifre :";
+            // 
+            // kullaniciaditxt
+            // 
+            this.kullaniciaditxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kullaniciaditxt.Location = new System.Drawing.Point(358, 66);
+            this.kullaniciaditxt.Name = "kullaniciaditxt";
+            this.kullaniciaditxt.Size = new System.Drawing.Size(100, 30);
+            this.kullaniciaditxt.TabIndex = 2;
+            // 
+            // sifretxt
+            // 
+            this.sifretxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifretxt.Location = new System.Drawing.Point(358, 111);
+            this.sifretxt.Name = "sifretxt";
+            this.sifretxt.Size = new System.Drawing.Size(100, 30);
+            this.sifretxt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(297, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Giriş Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sifretxt);
+            this.Controls.Add(this.kullaniciaditxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox kullaniciaditxt;
+        private System.Windows.Forms.TextBox sifretxt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OdevDizi/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdevDizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] kullanicigirisleri = new String[4];
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String[] kullanicigirisleri = new String[4];
+            kullanicigirisleri[0] = "Fatma";
+            kullanicigirisleri[1] = "123";
+            kullanicigirisleri[2] = "Kayra";
+            kullanicigirisleri[3] = "456";
+
+            if (kullaniciaditxt.Text == "Fatma" && sifretxt.Text == "123")
+            {
+                kullaniciaditxt.Clear();
+                sifretxt.Clear();
+                MessageBox.Show("Giriş Başarılı");
+            }
+            else if (kullaniciaditxt.Text == "Kayra" && sifretxt.Text == "456")
+            {
+                kullaniciaditxt.Clear();
+                sifretxt.Clear();
+                MessageBox.Show("Giriş Başarılı");
+            }
+            else
+            {
+                MessageBox.Show("Böyle bir kullanıcı yok lütfen kayıt olunuz.");
+            }
+
+        }
+        
+        
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
